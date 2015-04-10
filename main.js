a4373a4bdaf51207d8b749ca88b6e46201b7fd3d
Gtskk <tttt6399998@126.com>
2015-04-10 09:53:08 +0800

    newest

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function spawn(service){
 	child.on('exit', function(code){
 		logger.errorlogger.error('子进程退出，状态码' + code);
 		if(code != 0){
-			if(code == 2)
+			if(code == 1 || code == 2)
 				setTimeout(spawn(service), 5000);// 延迟5秒以便之前的资源释放
 			else
 				spawn(service);
