3414d1d6ad1191ee3bd527e2471c13ad567e32de
Gtskk <tttt6399998@126.com>
2015-04-14 19:04:04 +0800

    修复天线驻留时间导致的问题

--- lib/rfid/NodeRfid.cs
+++ lib/rfid/NodeRfid.cs
@@ -249,17 +249,17 @@ namespace NodeRfid
                         if (!tagList.ContainsKey(key))//上次盘点数据不包含在本次数据中
                         {
                             int checkCount = (int)tagVal["count"];                         
-                            if(checkCount > 1)
+                            if(checkCount > 1 || ((Tag)tagVal["data"]).RSSI > -58)
                             {
                                 goneList.Add(key, tagVal);//将上次盘点数据放到离架数据中
                             }
-                            else
+                            /*else
                             {
-                                if(((Tag)tagVal["data"]).RSSI > -70)
+                                if(((Tag)tagVal["data"]).RSSI < -60)
                                 {
                                     goneList.Add(key, tagVal);//将上次盘点数据放到离架数据中
                                 }
-                            }
+                            }*/
                         }
                         else
                         {
